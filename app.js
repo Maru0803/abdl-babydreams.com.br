ca752c6878c496bb240dcbb93fe5db4c1fa33ec4
Maru <163323276+Maru0803@users.noreply.github.com>
2024-09-24 13:46:20 -0300

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use((req, res, next) => {
     const origin = req.get('host');
-    if (origin === 'localhost:3000') {
+    if (origin === 'www.abdl-babydreams.com.br') {
         next();
     } else {
         res.status(200).send('Acesso Negado');
